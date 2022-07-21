1dfcbe8c0c20adc842dbcc096588b3387f57c9f0
arzen <87223161+sapna4310@users.noreply.github.com>
2022-07-21 18:38:22 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+
+        <div className='px-2 justify-between items-center w-full h-full'>
+
+          <div className='flex items-center'>
+            
+            <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Brand.</h1>
+            
+            </div>  
+
+        </div>
+    
+         <MenuIcon className='w-5' />
+         
+        
+
+    </div>
+  )
+}
+
+export default Navbar
